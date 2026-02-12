5e4d0e091b862ddbaeda2a3d3251d96ddfa21c7e
Любомир Чекански <lzl70110@gmail.com>
2026-02-12 12:13:43 +0200

    Add project files.

---
+++ NewLoco.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NewLoco.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
